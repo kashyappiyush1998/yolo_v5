ce04a8dce5e56d3a2a92c408391c8cebfe4ac0ce
kashyappiyush1998 <kashyappiyush1998@gmail.com>
2021-03-08 18:15:13 +0530

    torch hub -> pip

--- object_detection.py
+++ object_detection.py
@@ -1,18 +1,23 @@
 import torch
 from PIL import Image
 import os
+from yolov5 import YOLOv5
 # Model
 
 class YoloV5:
     def __init__(self):
-        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5x', pretrained=True, force_reload=False)
+        # self.model_path = torch.hub.load('ultralytics/yolov5', 'yolov5x', pretrained=True, force_reload=False)
+        self.model_path = '/datadrive/weights/yolov5x.pt'
+        self.device = 'cpu'
+        self.model = YOLOv5(self.model_path, self.device)
 
     def get_object_dict_single_image(self, img):
         classes = {}
         imgs = []
+        img = Image.fromarray(img)
         img.filename = 'temp.png'
         imgs.append(img)
-        result = self.model(imgs)
+        result = self.model(imgs, size=1280, augment=True)
 
         for i, (img, pred) in enumerate(zip(result.imgs, result.pred)):
             if pred is not None:
@@ -24,8 +29,11 @@ class YoloV5:
 
     def get_object_dict_multiple_images(self, imgs):
         classes = {}
+        new_imgs = []
         for img in imgs:
+            img=Image.fromarray(img)
             img.filename = 'temp.png'
+            new_imgs.append(img)
         result = self.model(imgs)
 
         for i, (img, pred) in enumerate(zip(result.imgs, result.pred)):
